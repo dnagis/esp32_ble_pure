377a06b857d183c59200aa5a38e3e853b7da6630
dnagis <vincent.achard@gmail.com>
2017-12-24 10:20:35 +0100

    refonte dans l'idée de poster sur forum pour comprendre pourquoi je reçois aucun scan rsp

--- main/main.c
+++ main/main.c
@@ -30,12 +30,12 @@
 #include "btc_main.h"
 
 
-static const char *MON_TAG = "BLE_PURE";
+static const char *MY_TAG = "BLE_PURE";
 
 static esp_ble_scan_params_t ble_scan_params = {
     .scan_type              = BLE_SCAN_TYPE_ACTIVE,
     .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
-    .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ONLY_WLST,
+    .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
     .scan_interval          = 0x50,
     .scan_window            = 0x30
 };
@@ -53,24 +53,20 @@ static esp_ble_adv_params_t adv_params = {
 
 static uint8_t raw_adv_data[] = {0x02, 0x01, 0x06, 0x02, 0x0a, 0xeb, 0x03, 0x03, 0xab, 0xcd, 0xab, 0xcd, 0xee};
 
-//static uint8_t raw_scan_rsp_data[] = {0x0f, 0x09, 0x45, 0x53, 0x50, 0x5f, 0x47, 0x41, 0x54, 0x54, 0x53, 0x5f, 0x44, 0x45, 0x4d, 0x4f};
-
 static uint8_t raw_scan_rsp_data[] = {0x0f, 0x09, 0x4c, 0x41, 0x52, 0x4f, 0x51, 0x55, 0x45};
 
-static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) //ce que j'arrive pas à avoir en hci 'raw', écouter ce qui arrive...
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
 {
 	esp_err_t ret;
 	uint8_t *adv_data = NULL;
 	
 	esp_ble_gap_cb_param_t *p_data = (esp_ble_gap_cb_param_t *) param;
-	ESP_LOGI(MON_TAG, "On est dans la callback function to the gap module, event = %x", event); //type de event: esp_gap_ble_api.h
+	ESP_LOGI(MY_TAG, "We're in the cb func to the gap module, event = %x", event); //event types: see esp_gap_ble_api.h
 	
 	if (event == ESP_GAP_BLE_SCAN_RESULT_EVT) {		
-		/*récupérer data: defs de esp_ble_gap_cb_param_t dans esp_gap_ble_api.h. Quand scan result -> param->scan_rst.bda (esp_bd_addr_t)*/
-		esp_log_buffer_hex(MON_TAG, param->scan_rst.bda, sizeof(esp_bd_addr_t)); //adresse
-		ESP_LOGI(MON_TAG, "c'est du SCAN_RESULT_EVT de type %i", p_data->scan_rst.ble_evt_type);
-		ESP_LOGI(MON_TAG, "adv_data_len = %i", p_data->scan_rst.adv_data_len);
-		//ESP_LOGI(MON_TAG, "c'est du SCAN_RESULT_EVT de type %x", p_data->scan_rst.ble_evt_type);
+				esp_log_buffer_hex(MY_TAG, param->scan_rst.bda, sizeof(esp_bd_addr_t)); //bdaddr
+		ESP_LOGI(MY_TAG, "SCAN_RESULT_EVT of type %x", p_data->scan_rst.ble_evt_type);
+		ESP_LOGI(MY_TAG, "adv_data_len = %i", p_data->scan_rst.adv_data_len);
 		adv_data = p_data->scan_rst.ble_adv;
             printf("data: ");
             for (int j = 0; j < p_data->scan_rst.adv_data_len; j++) {
@@ -80,17 +76,16 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *par
 	}
 	
 	if (event == ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT) {
-		ESP_LOGI(MON_TAG, "c'est du SCAN_PARAM_SET_COMPLETE_EVT");
+		ESP_LOGI(MY_TAG, "it's a SCAN_PARAM_SET_COMPLETE_EVT");		
+		ESP_LOGI(MY_TAG, "+++++++status = %i ", p_data->scan_param_cmpl.status); //esp_bt_status_t
 		
-		ESP_LOGI(MON_TAG, "+++++++status = %i ", p_data->scan_param_cmpl.status); //esp_bt_status_t
-		
-		ret = esp_ble_gap_start_scanning(30); //duration en s
+		ret = esp_ble_gap_start_scanning(200); //duration in s
 	}
 	
 	if (event == ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT) {
-		ESP_LOGI(MON_TAG, "c'est du ADV_DATA_RAW_SET_COMPLETE_EVT");
+		ESP_LOGI(MY_TAG, "it's a ADV_DATA_RAW_SET_COMPLETE_EVT");
 			
-		ret = esp_ble_gap_start_advertising(&adv_params); //duration en s
+		ret = esp_ble_gap_start_advertising(&adv_params); //duration in s
 	}
 	
 }
@@ -98,8 +93,6 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *par
 void app_main()
 {
     esp_err_t ret;
-    uint16_t taille_wl;
-    esp_bd_addr_t bda_wl = {0x81,0x82,0x83,0x84,0x85,0x86};
     
     
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
@@ -107,51 +100,42 @@ void app_main()
     nvs_flash_init();
     ret = esp_bt_controller_init(&bt_cfg);
     if (ret) {
-        ESP_LOGE(MON_TAG, "%s enable controller failed\n", __func__);
+        ESP_LOGE(MY_TAG, "%s enable controller failed\n", __func__);
         return;
     }
 
     ret = esp_bt_controller_enable(ESP_BT_MODE_BTDM); //"Now only support BTDM" (components/bt/include/bt.h) donc si tu tentes BLE ça plante
     if (ret) {
-        ESP_LOGE(MON_TAG, "%s enable controller failed\n", __func__);
+        ESP_LOGE(MY_TAG, "%s enable controller failed\n", __func__);
         return;
     }
 
-    ESP_LOGI(MON_TAG, "%s init bluetooth", __func__);
+    ESP_LOGI(MY_TAG, "%s init bluetooth", __func__);
     ret = esp_bluedroid_init();
     if (ret) {
-        ESP_LOGE(MON_TAG, "%s init bluetooth failed", __func__);
+        ESP_LOGE(MY_TAG, "%s init bluetooth failed", __func__);
         return;
     }
     
     ret = esp_bluedroid_enable();
     if (ret) {
-        ESP_LOGE(MON_TAG, "%s enable bluetooth failed\n", __func__);
+        ESP_LOGE(MY_TAG, "%s enable bluetooth failed\n", __func__);
         return;
     }
     
     //register the scan callback function to the gap module
     if ((ret = esp_ble_gap_register_callback(esp_gap_cb)) != ESP_OK) {
-        ESP_LOGE(MON_TAG, "gap register error, error code = %x", ret);
+        ESP_LOGE(MY_TAG, "gap register error, error code = %x", ret);
         return;
     }
     
     
-    ret = esp_ble_gap_get_whitelist_size(&taille_wl);
-		ESP_LOGI(MON_TAG, "******retour de get wl sz AVANT update wl = %i avec ret = %i", taille_wl, ret);
-	ret = esp_ble_gap_update_whitelist(1, bda_wl); //bool add_remove, esp_bd_addr_t remote_bda
-		ESP_LOGI(MON_TAG, "******retour de update wl ret = %i", ret);
-		
-	//vTaskDelay(3000 / portTICK_PERIOD_MS); //en millisecondes
-	
-	ret = esp_ble_gap_get_whitelist_size(&taille_wl); //pipi de chat, aucun effet. Idée au lieu de poster dans le forum: essayer en commandes 'raw hci'
-		ESP_LOGI(MON_TAG, "******retour de get wl sz APRES update wl = %i avec ret = %i", taille_wl, ret);
     ret = esp_ble_gap_set_scan_params(&ble_scan_params);
-		ESP_LOGI(MON_TAG, "******retour de set scan param = %i", ret); //ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT généré
+		ESP_LOGI(MY_TAG, "******set scan param returns = %i", ret); //will generate ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT
  
     
-	ret = esp_ble_gap_config_scan_rsp_data_raw(raw_scan_rsp_data, 9); //uint8_t *raw_data, uint32_t raw_data_len, devrait générer ESP_GAP_BLE_SCAN_RSP_DATA_RAW_SET_COMPLETE_EVT
-    ret = esp_ble_gap_config_adv_data_raw(raw_adv_data, 13); //uint8_t *raw_data, uint32_t raw_data_len, va générer ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT
+	ret = esp_ble_gap_config_scan_rsp_data_raw(raw_scan_rsp_data, 9); //uint8_t *raw_data, uint32_t raw_data_len, will generate ESP_GAP_BLE_SCAN_RSP_DATA_RAW_SET_COMPLETE_EVT
+    ret = esp_ble_gap_config_adv_data_raw(raw_adv_data, 13); //uint8_t *raw_data, uint32_t raw_data_len, will generate ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT
  
  
 }
